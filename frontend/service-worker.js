c43accb848c3062c80b21cdbc0b5da3eb0abd27d
Lyubomir Kiprov <lyubo@limechain.tech>
2019-06-07 16:11:25 +0300

    Fix api client endpoints

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ff14427fcadee4bdb2e37c2c5361419.js"
+  "/precache-manifest.225295a1ee8f20cb4142e4e550745468.js"
 );
 
 workbox.clientsClaim();
