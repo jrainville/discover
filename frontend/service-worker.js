ce0f2b07514a82768ddbe47ea19fe2810a2d02b7
George Spasov <spasov05203@gmail.com>
2019-07-20 16:49:28 +0300

    Status 1.7 release

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ff14427fcadee4bdb2e37c2c5361419.js"
+  "/precache-manifest.9b726aeedbb396bff49a2d8307e7e95c.js"
 );
 
 workbox.clientsClaim();
